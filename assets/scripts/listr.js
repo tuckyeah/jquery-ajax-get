55ff9e6b31206852e5e4e694be23beaaf8b38ab1
Jeffrey Horn <jeffh@generalassemb.ly>
2016-01-28 12:12:51 -0500

    Add AJAX request to GET /items

--- assets/scripts/listr.js
+++ assets/scripts/listr.js
@@ -1,3 +1,20 @@
 'use strict';
 
-module.exports = true;
+let logResponseBody = function(responseBody) {
+  console.table(responseBody);
+};
+
+let logRequestError = function(requestObject) {
+  console.error(requestObject);
+};
+
+let getItems = function() {
+  $.ajax({
+    url: 'http://localhost:3000/items',
+    type: 'GET'
+  })
+  .done(logResponseBody)
+  .fail(logRequestError);
+};
+
+$(document).ready(getItems);
